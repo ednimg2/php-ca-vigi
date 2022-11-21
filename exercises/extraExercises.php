e87fbea2eb0f4a97471ee17d42ab59890b2c466e
Mindaugas <mindaugas.g@rrr.lt>
2022-11-21 22:07:54 +0200

    Extra exercises

--- exercises/extraExercises.php
+++ exercises/extraExercises.php
@@ -51,3 +51,67 @@ function calculateInterestSum(float $deposit, int $year, float $interest): float
 }
 
 //var_dump(calculateInterestSum(100, 5, 5));
+
+
+/*
+ * Parasykite funkcija, kuri isspausdintu fibonacci seka nuo 0 tiek kartu kiek ivedeme i funkcija $n
+ * Pridekite teisinga return tipa
+ * 0, 1, 1, 2, 3, 5, 8, 13
+ */
+
+/*function printFibonacci(int $n): void
+{
+    $number1 = 0;
+    $number2 = 1;
+
+    echo $number1 . PHP_EOL;
+    echo $number2 . PHP_EOL;
+    for ($i = 1; $i <= $n; $i++) {
+        $number3 = $number1 + $number2;
+        $number1 = $number2;
+        $number2 = $number3;
+        echo $number3 . PHP_EOL;
+    }
+}
+
+printFibonacci(10);*/
+
+/*function printFibonacci(int $n): void
+{
+    $i = 0;
+    $number1 = 0;
+    $number2 = 1;
+
+    echo $number1 . PHP_EOL;
+    echo $number2 . PHP_EOL;
+
+    while ($i < $n) {
+        $number3 = $number1 + $number2;
+        echo $number3 . PHP_EOL;
+        $number1 = $number2;
+        $number2 = $number3;
+        $i++;
+    }
+}
+
+printFibonacci(10);*/
+
+function getFibonacci(int $n): int
+{
+    if ($n === 0) {
+        return 0;
+    } elseif ($n === 1) {
+        return 1;
+    } else {
+        return (getFibonacci($n - 1) + getFibonacci($n - 2));
+    }
+}
+
+function printFibonacci($x)
+{
+    for ($i = 0; $i < $x; $i++) {
+        echo getFibonacci($i) . PHP_EOL;
+    }
+}
+
+printFibonacci(10);
