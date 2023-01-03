bba0feb8ce6bc8db17bcbb1ec165354bea139aeb
Mindaugas <mindaugas.g@rrr.lt>
2023-01-03 17:55:16 +0200

    Calculator

---
+++ exercises/oop/calculator.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Sukurti Calculator class'e, kuri moketu:
+ * Properties:
+ * - numberA
+ * - numberB
+ * Methods:
+ * + sum() //sudeti
+ * + subtract() //atimti
+ * + multiply() // sudauginti
+ * + divide() // dalinti
+ *
+ * Kodo vykdymas.
+ *
+ * $calculator = new Calculator(6, 3);
+ *
+ * $calculator->sum(); // 9
+ * $calculator->subtract(); // 3
+ * $calculator->multiply(); // 18
+ * $calculator->divide(); // 2
+ *
+ * Pakeiskite koda taip, kad numberA ir numberB reiktu uzsetinti per setter'ius
+ *
+ * Kodo vykdymas.
+ *
+ * $calculator = new Calculator();
+ * $calculator->setNumberA(6);
+ * $calculator->setNumberB(3);
+ *
+ * $calculator->sum(); // 9
+ */
